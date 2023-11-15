84b3a973b33dec0415731a669fb4d95cd3157bad
Igor Seliverstov <igrseliwjorstow@bk.ru>
2023-11-15 08:55:21 +0300

    апдейты + докер

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import {Telegraf, Markup} from 'telegraf';
+import { message } from "telegraf/filters"
 
 const token = '6340439412:AAEFAvAm8ugA90awEH7i-auv9aYoAOmhEbc';
-const webAppUrl = 'https://tg-angular-app.web.app/';
+const webAppUrl = 'https://tg-angular-app.web.app';
 
 const bot = new Telegraf(token);
 
@@ -11,10 +12,15 @@ bot.command('start', (ctx) => {
         Markup.keyboard([
             Markup.button.webApp(
                 'Отправить сообщение',
-                webAppUrl
+                webAppUrl + '/feedback'
             )
         ])
     )
+})
+
+bot.on(message('web_app_data'), async (ctx) => {
+    const data = ctx.webAppData.data.json();
+    ctx.reply(`Сообщение ${data?.feedback ?? ""}  принято`);
 })
 
 bot.launch();
